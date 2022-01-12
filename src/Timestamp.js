72497a6d0986fd31b6dd6fcb12234e69d4970552
shaki-0 <shakiobadina28@gmail.com>
2022-01-12 16:33:49 +0000

    Semicolon bug removed
    Noticed a bug for the h1 and h2, The bug was a semicolon after the closing tags. Once I removed the bug it completely changed the spacing adjustments that were made beforehand. I reverted back to setting the margins to 29px auto for the h1 and h2. I also changed the margin for the temperature border back to 30px.

--- src/Timestamp.js
+++ src/Timestamp.js
@@ -4,7 +4,7 @@ import "./Timestamp.css";
 export default function Timestamp() {
   return (
     <div className="Timestamp">
-      <h2 class="d-flex mt-1 justify-content-center"> Saturday 11:49 </h2>;
+      <h2> Saturday 11:49 </h2>
     </div>
   );
 }
