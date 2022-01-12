a47e782de4cd75771500fe67781f1b4d8d31a602
shaki-0 <shakiobadina28@gmail.com>
2022-01-12 15:55:42 +0000

    Removing space between h1 and h2
    1. Noticed the spacing between the Cities component and Timestamp component was too wide.
    2. For the Cities component, in the h1 I set the margin to 0
    3, I used Bootstrap utility classes to add a margin top between the h1 and the search bar.
    4. I removed the margin that was placed for the h2 in the Timestamp component.

--- src/Cities.js
+++ src/Cities.js
@@ -5,7 +5,7 @@ import "./styles.css";
 export default function Cities() {
   return (
     <div className="Cities">
-      <h1> London </h1>;
+      <h1 className="mt-5"> London </h1>;
     </div>
   );
 }
