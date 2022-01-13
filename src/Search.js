a24745d72b4a342fa6dc0142602e6178633a1c91
shaki-0 <shakiobadina28@gmail.com>
2022-01-13 15:07:18 +0000

    Made the search engine work
    1. For the search engine to work and display the results of the temperature when a user types in a city I created a new state called city and setCity.
    2. In the parenthesis I put the property props.default city and removed default city from the apiUrl. In the apiUrl I replaced the property with the state city.
    3.For the search engine to work I used to event listeners. For the first event listener I placed it in the form open tag and used the event listener onSumbit.
    4. I then created a function called handlesumbit in order to prevent the default behaviour.
    5. For the second event listnener I placed it in the input and used onChange. This means when a user searches for a city, the city the person types will change.
    6. I created a function called updateCity. Since I do not want to prevent the default behaviour but want to store the city the user types I used event.target.value instead preened with the state setCity.
    7.To store the api call I created a new function called search.

--- src/Search.js
+++ src/Search.js
@@ -7,6 +7,7 @@ import Timestamp from "./Timestamp";
 
 export default function Search(props) {
   const [weatherData, setWeatherData] = useState({ load: false });
+  const [city,setCity] = useState(props.defaultCity);
   function getResponse(response) {
     setWeatherData({
       load: true,
@@ -20,14 +21,30 @@ export default function Search(props) {
     });
   }
 
+ function search() {
+ let apiKey = "d7e8260f13d9346a2e404a24c9758f9c";
+ let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+ axios.get(apiUrl).then(getResponse);
+ }
+
+  function handleSubmit (event) {
+    event.preventDefault ();
+    search ();
+  }
+
+   function updateCity(event) {
+     setCity(event.target.value);
+   }
+
   if (weatherData.load) {
     return (
-      <form className="Search">
+      <form  onSubmit={handleSubmit} className="Search">
         <div className="input-group mb-3">
           <input
             type="search"
             className="form-control"
             placeholder="Type a city"
+            onChange = {updateCity}
           />
           <div className="input-group-append">
             <button className="btn btn-outline-secondary" type="button">
@@ -41,9 +58,7 @@ export default function Search(props) {
       </form>
     );
   } else {
-    let apiKey = "d7e8260f13d9346a2e404a24c9758f9c";
-    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${props.defaultCity}&appid=${apiKey}&units=metric`;
-    axios.get(apiUrl).then(getResponse);
+    search();
     return "Loading...";
   }
 }
